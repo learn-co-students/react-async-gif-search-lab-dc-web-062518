056be67f5f989b06d6a51625e6fb1f41900aa61f
github username <github email address>
2018-09-01 16:08:07 -0400

    Done.

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -4,12 +4,15 @@ export default class GifList extends Component{
   render(){
     // var GifPlayer = require('react-gif-player')
     // var Gif = require('react-gif');
+    // <GifPlayer src={require(this.props.gifs[0].images.fixed_height_still)} />
+    // <GifPlayer src={require(this.props.gifs[1].images.fixed_height_still)} />
+    // <GifPlayer src={require(this.props.gifs[2].images.fixed_height_still)} />
     return(
       <div>
         {console.log(this.props.gifs[0].images.fixed_height_still)}
-        // <GifPlayer src={require(this.props.gifs[0].images.fixed_height_still)} />
-        // <GifPlayer src={require(this.props.gifs[1].images.fixed_height_still)} />
-        // <GifPlayer src={require(this.props.gifs[2].images.fixed_height_still)} />
+        <h1>GIF1</h1>
+        <h1>GIF2</h1>
+        <h1>GIF3</h1>
 
       </div>
     )
